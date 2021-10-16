bf4a01bd95c0244db5369f62dce17ee70108b110
prithwi <prethwiraj06@gmail.com>
2021-10-16 19:49:39 +0530

    initial commit

---
+++ src/app/login/login.page.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: 'login.page.html',
+  styleUrls: ['login.page.scss'],
+})
+export class LoginPage implements OnInit{
+  passwordType: string = 'password';
+  passwordIcon: string = 'eye-off';
+  invalidCredentials: boolean = false;
+
+  loginForm: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.loginForm = this._formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required]]
+    })
+  }
+
+  hideShowPassword() {
+    this.passwordType = this.passwordType === 'text' ? 'password' : 'text';
+    this.passwordIcon = this.passwordIcon === 'eye-off' ? 'eye' : 'eye-off';
+  }
+  
+  onFormSubmit() {
+    console.log(this.loginForm.value)
+  }
+
+}
